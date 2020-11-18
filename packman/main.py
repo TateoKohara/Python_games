563929abad09a534254dd364dac6827621173487
TateoKohara <68531009+TateoKohara@users.noreply.github.com>
2020-11-18 10:43:15 +0900

    Update main.py

--- packman/main.py
+++ packman/main.py
@@ -1,3 +1,33 @@
 import pygame
 from game import Game
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 576
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    pygame.display.set_caption("PACKMAN")
+
+    done = False
+
+    clock = pygame.time.CLock()
+
+    game = Game()
+
+    while not done:
+        done = game.process_events()
+
+        game.run_logic()
+
+        game.display_frame(screen)
+
+        clock.tick(30)
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()        
